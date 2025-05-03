6071a3b23a56ca9d255c546aab6622b4fe4791dd
Justus <galaxycheesemc@gmail.com>
2025-05-03 09:51:48 -0700

    Details and styling

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
-  templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
+  templateUrl: './contact-detail.component.html'
 })
 export class ContactDetailComponent {
 
