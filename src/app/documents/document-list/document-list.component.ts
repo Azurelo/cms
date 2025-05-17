f541479f443a2728093fa0cfe3a6ecac58280199
Justus <galaxycheesemc@gmail.com>
2025-05-17 08:32:51 -0700

    document lsit and item setup

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
   documents: Document[] = [
-    new Document('1', 'Doc 1', 'Description 1', 'http://example.com/doc1', null),
-    new Document('2', 'Doc 2', 'Description 2', 'http://example.com/doc2', null)
+    new Document('1', 'Document A', 'Description A', 'http://example.com/docA', null),
+    new Document('2', 'Document B', 'Description B', 'http://example.com/docB', null),
+    new Document('3', 'Document C', 'Description C', 'http://example.com/docC', null),
+    new Document('4', 'Document D', 'Description D', 'http://example.com/docD', null),
   ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
