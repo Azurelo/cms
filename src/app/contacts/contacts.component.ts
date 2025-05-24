5e84fdd4663fca92599c09ca174eb11446e0165a
Justus <galaxycheesemc@gmail.com>
2025-05-24 10:42:36 -0700

    contacts service

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { Contact } from './contact.model'; 
-
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
   selectedContact: Contact;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contactService.contactSelectedEvent.subscribe(contact => {
+      this.selectedContact = contact;
+    });
+  }
 }
