38377ba22505e7dbba82bab4462d144b17665b6b
Justus <galaxycheesemc@gmail.com>
2025-06-05 12:27:11 -0700

    Document observables

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { DocumentService } from '../document.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
+  styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
+export class DocumentListComponent implements OnInit, OnDestroy {
   documents: Document[] = [];
+  subscription: Subscription;
 
   constructor(private documentService: DocumentService) {}
 
-  ngOnInit() {
-    this.documentService.documentChangedEvent.subscribe((documents: Document[]) => {
-      this.documents = documents;
-    });
-
+  ngOnInit(): void {
     this.documents = this.documentService.getDocuments();
+    this.subscription = this.documentService.documentListChangedEvent
+      .subscribe((documents: Document[]) => {
+        this.documents = documents;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -1,31 +1,65 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Document } from './document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 
 @Injectable({ providedIn: 'root' })
 export class DocumentService {
   documents: Document[] = [];
-  documentSelectedEvent = new EventEmitter<Document>();
+  documentListChangedEvent = new Subject<Document[]>();
+  maxDocumentId: number;
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments(): Document[] {
     return this.documents.slice();
   }
 
-  getDocument(id: string): Document | null {
-    return this.documents.find(d => d.id === id) || null;
+  getDocument(id: string): Document {
+    return this.documents.find(d => d.id === id);
   }
-  documentChangedEvent = new EventEmitter<Document[]>();
 
-  deleteDocument(document: Document) {
+  getMaxId(): number {
+    let maxId = 0;
+    this.documents.forEach(document => {
+      const currentId = parseInt(document.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    });
+    return maxId;
+  }
+
+  addDocument(newDocument: Document): void {
+    if (!newDocument) return;
+
+    this.maxDocumentId++;
+    newDocument.id = this.maxDocumentId.toString();
+    this.documents.push(newDocument);
+    this.documentListChangedEvent.next(this.documents.slice());
+  }
+
+  updateDocument(original: Document, updated: Document): void {
+    if (!original || !updated) return;
+
+    const pos = this.documents.indexOf(original);
+    if (pos < 0) return;
+
+    updated.id = original.id;
+    this.documents[pos] = updated;
+    this.documentListChangedEvent.next(this.documents.slice());
+  }
+
+  deleteDocument(document: Document): void {
     if (!document) return;
+
     const pos = this.documents.indexOf(document);
     if (pos < 0) return;
-    this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit(this.documents.slice());
-  }
 
+    this.documents.splice(pos, 1);
+    this.documentListChangedEvent.next(this.documents.slice());
+  }
 }
